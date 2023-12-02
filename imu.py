215a58a2f251b94d65f1eaf811098fb0027216fd
Anvitha Ramachandran <44482134+anvitha305@users.noreply.github.com>
2023-12-02 18:04:49 -0500

    Update imu.py

--- imu.py
+++ imu.py
@@ -4,12 +4,10 @@ import time
 arduino = serial.Serial(port='COM3', baudrate=9600, timeout=.1)
 
 def read_imu():
-	orientation = arduino.readline()
-	angvel = arduino.readline()
-	linaccel = arduino.readline()
-	mag = arduino.readline()
 	acc = arduino.readline()
-	return orientation, angvel, linaccel, mag, acc
+	mag = arduino.readline()
+	gyro = arduino.readline()
+	return orientation, acc, mag, gyro;
 while True: 
 	for i in read_imu():
 		print(i)
