420ed2aaf5a655f2721eb777e62d13dd80fd911a
Anvitha Ramachandran <44482134+anvitha305@users.noreply.github.com>
2023-12-02 19:36:41 -0500

    better and newer imu code :))

--- bno055read.ino
+++ bno055read.ino
@@ -32,7 +32,7 @@ uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
 
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
 //                                   id, address
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
+Adafruit_BNO055 bno = Adafruit_BNO055(55,0x28, &Wire);
 
 void setup(void)
 {
@@ -41,7 +41,6 @@ void setup(void)
   while (!Serial) delay(10);  // wait for serial port to open!
 
   Serial.println("Orientation Sensor Test"); Serial.println("");
-
   /* Initialise the sensor */
   if (!bno.begin())
   {
