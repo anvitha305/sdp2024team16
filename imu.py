f7a4ffaa260ea5e662316daeac3ee50c1e6ee332
Anvitha Ramachandran <44482134+anvitha305@users.noreply.github.com>
2023-12-02 20:25:13 -0500

    Add files via upload

--- imu.py
+++ imu.py
@@ -1,13 +1,29 @@
 import serial
 import time
 
-arduino = serial.Serial(port='COM3', baudrate=9600, timeout=.1)
+arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
 
 def read_imu():
-	acc = arduino.readline()
+	orientation = arduino.readline()
+	angvel = arduino.readline()
+	linaccel = arduino.readline()
 	mag = arduino.readline()
-	gyro = arduino.readline()
-	return orientation, acc, mag, gyro;
-while True: 
+	acc = arduino.readline()
+	return orientation, angvel, linaccel, mag, acc
+#while True: 
+def get_data():
+	results = dict()
 	for i in read_imu():
-		print(i)
+		if i is not bytes(): 
+			try:
+				res = i.strip().decode()
+				crds = list(map(lambda x: float(x.strip()[3:]), res[5:].split('|')))
+				print(crds)
+				if len(crds)!= 3:
+					raise ValueError()
+				results[res[0:3]] = crds
+				print(results)
+			except ValueError:
+				pass
+	if len(results)==5:
+		yield results
